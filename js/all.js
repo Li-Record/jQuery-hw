de50c5258e3abe3fb9f58b1433061c56fcba870d
White12343 <white11190709@gmail.com>
2019-05-18 15:53:52 +0800

    輪播+menu

--- js/all.js
+++ js/all.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-	
+    $('.menu-item').on('click', '.manu-item-link>li>a', function(event) {
+        event.preventDefault();
+        /* Act on the event */
+        $(this).find('.menu-item-arrow').slideToggle();
+        $(this).parent().find('ul').slideToggle();
+        $(this).parent().siblings().find('ul').slideUp();
+        $(this).toggleClass('active');
+        $(this).parent().siblings().find('a').removeClass('active');
+    });
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        loop: true,
+        speed: 2000,
+        autoplay: {
+            delay: 3000,
+        },
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    })
 });
